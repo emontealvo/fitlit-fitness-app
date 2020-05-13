4f55894eb43b40af2f6b1ef200bbcca7d63cd3b7
Jeremiah Black <jeremiahblackol@gmail.com>
2020-05-13 17:04:43 -0600

    generates user info and greets user

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,27 @@
+/* eslint-disable max-len */
 console.log("Hello World");
 
+let user;
+let userRepository;
+
+
 window.onload = () => {
-  
+  userRepository = new UserRepository(userData)
+  instantiateUser()
+  greetUser()
 }
 
-function generateUser() {
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 49 + 1)
+}
 
+const instantiateUser = () => {
+  let randomUser = userRepository.findUser(generateRandomNumber())
+  user = new User (randomUser.id, randomUser.name, randomUser.address, randomUser.email, randomUser.strideLength, randomUser.dailyStepGoal, randomUser.friends)
+  return user
+}
+
+const greetUser = () => {
+  let greetingSection = document.querySelector('.greeting')
+  greetingSection.innerText = `Howdy ${user.name}`
 }
